af547401cd342b506d112374ba1f068f4fda06bc
Joshua <atsujoshua67@gmail.com>
2023-10-05 22:13:48 -0700

    Hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - the starting point
+ *
+ * Return: Always 0 and success
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
