3ef8fa490f9e068b5351e3905dad0c11e45dd7d7
Joshua <atsujoshua67@gmail.com>
2023-10-05 22:21:34 -0700

    Hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - the starting point of c
+ *
+ * Return: always 0 and nerver ever
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
