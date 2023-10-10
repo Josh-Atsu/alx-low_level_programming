6d9f45f676b166b0da9e8b379b0cc6e408066527
Joshua <atsujoshua67@gmail.com>
2023-10-09 21:54:11 -0700

    nunbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - the project starts
+ *
+ * Return: - ends here with 0
+ */
+
+int main(void)
+{
+	int z;
+
+	for (z = 0; z <= 9; z++)
+	{
+		putchar(z);
+	}
+	putchar('\n');
+
+	return (0);
+}
