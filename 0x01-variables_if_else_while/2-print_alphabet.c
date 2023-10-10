748a7aef5af5a3abd3dfd99bbb1d60d2b9bf0f9e
Joshua <atsujoshua67@gmail.com>
2023-10-09 18:27:41 -0700

    Printing small alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - The starting point of the program
+ *
+ * Return: - SUccessful 0
+ */
+
+int main()
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+
+	return (0);
+}
