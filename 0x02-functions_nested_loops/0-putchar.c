d7dcd8532083078d1fe930dfa867accc994f67f0
Joshua <atsujoshua67@gmail.com>
2023-10-10 21:13:50 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (successful)
+ */
+
+int main(void)
+{
+	{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	}
+
+	return (0);
+}
