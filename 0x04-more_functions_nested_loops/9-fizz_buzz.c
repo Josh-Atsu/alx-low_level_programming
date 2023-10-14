ea5a2d92bfd4719d1c7a5c136057aba36114a3a0
Joshua <atsujoshua67@gmail.com>
2023-10-13 20:21:10 -0700

    Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - The check starts
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int x;
+
+	x = 1;
+	while (x <= 100)
+	{
+		if (x % 15 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (x % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (x % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else
+		{
+			printf("%d ", x);
+		}
+		x++;
+	}
+	printf("\n");
+
+	return (0);
+}
