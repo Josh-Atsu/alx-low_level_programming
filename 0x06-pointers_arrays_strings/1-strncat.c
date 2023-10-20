deb2f1008a23a1311fc6dbeee3dd8786af694afb
Joshua <atsujoshua67@gmail.com>
2023-10-19 19:36:56 -0700

    combine a number of characters

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenate certain numbewr of characters
+ * @dest: Has the main string to be added
+ * @src: Contains string to be concatenated
+ * @n: holds the number of characters to concatenate
+ *
+ * Return: the concatenated string, dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int dlen;
+	int j;
+	int slen;
+
+	dlen = 0;
+	for (i  = 0; dest[i] != '\0'; i++)
+	{
+		dlen++;
+	}
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		slen++;
+	}
+
+	j = 0;
+	while (j < n && j < slen)
+	{
+		dest[dlen] = src[j];
+		j++;
+		dlen++;
+	}
+	dest[dlen] = '\0';
+	return (dest);
+}
