c802692d9bba43f1876b6c888cc5fe8ea88abf24
Joshua <atsujoshua67@gmail.com>
2023-10-24 22:57:38 -0700

    Double pointer, double fun

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * set_string - Add up to string
+ * @s: has the string to be added up
+ * @to: second para
+ *
+ * Return: the real string
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
