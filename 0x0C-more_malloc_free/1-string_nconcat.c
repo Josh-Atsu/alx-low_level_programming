0b47b257f7f391a75262d0df37dcd24173c0e7e8
Joshua <atsujoshua67@gmail.com>
2023-11-03 14:50:29 -0700

    malloc and cancat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,42 +12,43 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int i;
 	char *str;
-	unsigned int len_s2 = 0, len_s1 = 0;
+	unsigned int len_s2, len_s1;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	for (i = 0; s2[i] != '\0'; i++)
-	{
-		len_s2++;
-	}
+	/**for (i = 0; s2[i] != '\0'; i++)
+	*{
+	*	len_s2++;
+	*}
+	*/
+	len_s1 = strlen(s1);
+	len_s2 = strlen(s2);
 	if (n >= len_s2)
 	{
-		str = malloc(sizeof(char) * (strlen(s1) + strlen(s2) + 1));
+		str = malloc(sizeof(char) * (len_s1 + len_s2 + 1));
 	}
 	else
 	{
-		str = malloc(sizeof(char) * (strlen(s1) + (n + 1)));
+		str = malloc(sizeof(char) * (len_s1 + n + 1));
 	}
 	if (str == NULL)
 	{
 		free(str);
 		return (NULL);
 	}
-	for (i = 0; s1[i] != '\0'; i++)
+	for (i = 0; i < len_s1; i++)
 	{
 		str[i] = s1[i];
-		len_s1++;
 	}
 	for (i = 0; i < n; i++)
 	{
 		if (s2[i] != '\0')
 		{
-			str[len_s1] = s2[i];
-			len_s1++;
+			str[len_s1 + i] = s2[i];
 		}
 	}
-	str[len_s1] = '\0';
+	str[len_s1 + i] = '\0';
 	return (str);
 }
