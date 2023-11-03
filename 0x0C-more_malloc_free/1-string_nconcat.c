70f24bf0811fe037f5ad64256a007189f9486361
Joshua <atsujoshua67@gmail.com>
2023-11-02 22:23:02 -0700

    memory allocate and concat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,9 +14,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int len_s2 = 0, len_s1 = 0;
 
 	if (s1 == NULL)
-		s1 = "";
+		len_s1 = 0;
 	if (s2 == NULL)
-		s2 = "";
+		len_s2 = 0;
 	for (i = 0; s2[i]; i++)
 	{
 		len_s2++;
