4613a2a633e1438ef9bbc2fc0c81004ff4d363c1
Joshua <atsujoshua67@gmail.com>
2023-11-03 22:58:01 -0700

    mul

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -9,5 +9,10 @@ int *array_range(int min, int max);
 void ximple_print_buffer(int *buffer, unsigned int size);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 /*void simple_print_buffer(char *buffer, unsigned int size);*/
+int main(int argc, char *argv[]);
+int _strlen(char *s);
+void error(void);
+int is_digit(char *c);
+int _putchar(char c);
 
 #endif
