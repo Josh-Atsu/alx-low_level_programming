59690a0d38b5eca017ac138066f5139e9a0b4fb1
Joshua <atsujoshua67@gmail.com>
2023-11-06 19:22:33 -0800

    dog _ H

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - poppy
+ * @name: name of dogs
+ * @age: age of dogs
+ * @owner: owner of dogs
+ *
+ * Description: a new type struct dog with the above elements
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+
+};
+
+#endif
