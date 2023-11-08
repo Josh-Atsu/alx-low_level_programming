36f20ff6d45d1f08738bf56d194e4f715372fc9f
Joshua <atsujoshua67@gmail.com>
2023-11-08 14:24:14 -0800

    A function that print each element of an array of a function

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * array_iterator - loop through the array and print each member
+ * @array: the array of integers
+ * @size: size of array
+ * @action: pointer to the function that needs to be used
+ *
+ * Return: a pointer to the function
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (action == 0)
+		return;
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
