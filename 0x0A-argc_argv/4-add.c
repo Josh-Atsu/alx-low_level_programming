a42d6b60389c76c31e9d23ce1b0e4731aa892c12
Joshua <atsujoshua67@gmail.com>
2023-11-18 03:40:37 -0800

    add all numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,19 +10,23 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, n, add = 0;
+	int i, j, n, add = 0;
 
 	if (argc != 1)
 	{
 		for (i = 1; i < argc; i++)
 		{
-			n = atoi(argv[i]);
-			/*printf("%d\n", n);*/
-			if ((isdigit(n)))
+			for (j = 0; argv[i][j] != '\0'; j++)
 			{
-				printf("Error\n");
-				return (1);
+			/*n = atoi(argv[i]);*/
+			/*printf("%d\n", n);*/
+				if (!(isdigit(argv[i][j])))
+				{
+					printf("Error\n");
+					return (1);
+				}
 			}
+			n = atoi(argv[i]);
 			add += n;
 		}
 	}
