850d943e05906baf3dfbbd1c0c3891a0e15de1c0
Joshua <atsujoshua67@gmail.com>
2023-11-21 09:29:50 -0800

    memory allocate for grid space

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -26,7 +26,7 @@ int **alloc_grid(int width, int height)
 	for (i = 0; i < height; i++)
 	{
 		m[i] = malloc(sizeof(int) * width);
-		if (m == NULL)
+		if (m[i] == NULL)
 		{
 			for (; i >= 0; i--)
 			{
