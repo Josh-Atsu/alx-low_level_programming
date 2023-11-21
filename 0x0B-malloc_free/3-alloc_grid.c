c24b5617d4185aef066458543ba25b9e72d2e086
Joshua <atsujoshua67@gmail.com>
2023-11-21 10:49:44 -0800

    allocate memory of 2 dimension

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -36,9 +36,10 @@ int **alloc_grid(int width, int height)
 				while (i >= 0)
 				{
 					free(m[j]);
-					free(m);
-					i--, j--;
+				
+					j--;
 				}
+				free(m);
 				return (NULL);
 			}
 		}
