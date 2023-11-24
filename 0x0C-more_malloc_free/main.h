cd9df61b7413da4d6303f94702b3ccc7ab95bad7
Joshua <atsujoshua67@gmail.com>
2023-11-24 15:50:10 -0800

    calloc

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -4,5 +4,6 @@
 void *malloc_checked(unsigned int n);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
+void simple_print_buffer(char *buffer, unsigned int size);
 
 #endif
