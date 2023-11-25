c48b6b65111d7b9d2a2e5043b663580d15845212
Joshua <atsujoshua67@gmail.com>
2023-11-25 11:23:15 -0800

    array range from minimun to maximun

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * array_range - an array from min to max inclusively
+ * @min: minimum number
+ * @max: maximun number
+ * Return: pointer to the allocated space of memory
+ */
+int *array_range(int min, int max)
+{
+	int *m;
+	int i, numlen;
+
+	if (min > max)
+		return (NULL);
+	numlen = (max - min) + 1;
+	m = malloc(numlen * sizeof(int));
+	for (i = 0; i < numlen; i++)
+	{
+		m[i] = min++;
+	}
+	return (m);
+}
