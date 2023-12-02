263ef81673f3781bd0f79251d539b86906d5acb2
Joshua <atsujoshua67@gmail.com>
2023-12-01 23:50:23 -0800

    Print all numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,8 +11,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list all_num;
 
-	if (n == 0)
-		return;
 	va_start(all_num, n);
 	for (i = 0; i < n; i++)
 	{
