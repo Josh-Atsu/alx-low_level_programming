5aeecae495d1fec9883778da3831fdd509622c87
Joshua <atsujoshua67@gmail.com>
2023-12-02 00:17:23 -0800

    Print all strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -22,6 +22,8 @@ void print_strings(const char *separator, const unsigned int n, ...)
 				if (separator != NULL && i < n - 1)
 					printf("%s", separator);
 			}
+			else
+				printf("(nil)");
 		}
 	}
 	printf("\n");
