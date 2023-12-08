79ed37f65ca421ca307352d3e662cdec86b29b17
Joshua <atsujoshua67@gmail.com>
2023-12-07 17:34:51 -0800

    return the number of nodes

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements of list_t h
+ * @h: the struct list_s
+ * Return: the number of elements
+ */
+size_t list_len(const list_t *h)
+{
+	int count = 1;
+
+	while (h->next != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
