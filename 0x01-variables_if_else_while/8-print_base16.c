0a6db2074b764b7173bbbcafaadaf267e9857c80
Joshua <atsujoshua67@gmail.com>
2023-12-10 15:04:08 -0800

    deleting unwanted files

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,17 +9,5 @@
 int main(void)
 {
 	int vex;
-	char rex;
 
-	for (vex = 0; vex <= 9; vex++)
-	{
-		putchar(vex + '0');
-	}
-	for (rex = 'a'; rex <= 'f'; rex++)
-	{
-		putchar(rex);
-	}
-	putchar('\n');
-
-	return (0);
-}
+	for 
