e5fda9c2a1a62cdbcfff13eb4f9be59e9e57c16f
Joshua <atsujoshua67@gmail.com>
2023-12-12 15:20:07 -0800

    inserting node at index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,7 +20,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		node_ptr = node_ptr->next;
 	}
 	node_ptr = *head; /* reassign head to node_ptr */
-	if (idx > c_node)
+	if (idx > c_node + 1)
 		return (NULL);
 	temp = malloc(sizeof(listint_t));
 	if (temp == NULL)
