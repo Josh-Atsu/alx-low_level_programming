3902a07c029c03dfd5afaecdef2a3f3930889248
Joshua <atsujoshua67@gmail.com>
2023-12-13 15:14:57 -0800

    Reversing a listint

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -6,7 +6,35 @@
  */
 listint_t *reverse_listint(listint_t **head)
 {
+	listint_t *node_ptr, *ptr_node, *temp;
+
 	if (head == NULL || *head == NULL)
 		return (NULL);
+	node_ptr = *head;
+	ptr_node = NULL;
+	if (node_ptr->next == NULL)
+		return (*head);
+	node_ptr = node_ptr->next;
+	ptr_node = *head;
+	*head = node_ptr;
+	ptr_node->next = NULL;
+	temp = node_ptr->next;
+	node_ptr->next = ptr_node;
+	node_ptr = temp;
+
+	while (node_ptr->next != NULL)
+	{
+		ptr_node = *head;
+		*head = node_ptr;
+		node_ptr = node_ptr->next;
+		temp->next = ptr_node;
+		temp = node_ptr;
+	}
+	ptr_node = *head;
+	*head = node_ptr;
+	node_ptr->next = ptr_node;
+	ptr_node = NULL;
+	node_ptr = NULL;
+	temp = NULL;
 	return (*head);
 }
