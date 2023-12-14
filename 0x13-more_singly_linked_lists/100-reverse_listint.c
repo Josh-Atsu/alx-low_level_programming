9134ce6a06115193d1e686408c84944f23182ed0
Joshua <atsujoshua67@gmail.com>
2023-12-13 16:11:39 -0800

    reversing a listint

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -6,35 +6,22 @@
  */
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *node_ptr, *ptr_node, *temp;
+	listint_t *node_ptr, *ptr_node;
 
 	if (head == NULL || *head == NULL)
 		return (NULL);
-	node_ptr = *head;
-	ptr_node = NULL;
-	if (node_ptr->next == NULL)
+	if ((*head)->next == NULL)
 		return (*head);
-	node_ptr = node_ptr->next;
-	ptr_node = *head;
-	*head = node_ptr;
-	ptr_node->next = NULL;
-	temp = node_ptr->next;
-	node_ptr->next = ptr_node;
-	node_ptr = temp;
-
-	while (node_ptr->next != NULL)
+	node_ptr = *head;
+	*head = (*head)->next;
+	node_ptr->next = NULL;
+	while ((*head)->next != NULL)
 	{
-		ptr_node = *head;
-		*head = node_ptr;
-		node_ptr = node_ptr->next;
-		temp->next = ptr_node;
-		temp = node_ptr;
+		ptr_node = (*head)->next;
+		(*head)->next = node_ptr;
+		node_ptr = *head;
+		*head = ptr_node;
 	}
-	ptr_node = *head;
-	*head = node_ptr;
-	node_ptr->next = ptr_node;
-	ptr_node = NULL;
-	node_ptr = NULL;
-	temp = NULL;
+	(*head)->next = node_ptr;
 	return (*head);
 }
