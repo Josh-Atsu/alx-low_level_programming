076be0f2cdffbaf6e98fe89f6ba6afd42156021d
Joshua <atsujoshua67@gmail.com>
2024-02-07 06:49:21 -0800

    a function that creates a new dog.

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -34,6 +34,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(m);
 		return (NULL);
 	}
+	for (i = 0; name[i]; i++)
+                m->name[i] = name[i];
 	m->age = age;
 	for (j = 0; owner[j]; j++)
 		;
@@ -45,8 +47,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(m);
 		return (NULL);
 	}
-	for (i = 0; name[i]; i++)
-		m->name[i] = name[i];
 	for (j = 0; owner[j]; j++)
 		m->owner[j] = owner[j];
 	return (m);
