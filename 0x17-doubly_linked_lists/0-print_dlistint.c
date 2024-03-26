fcf14a2452e3c33967a5b7ef90d731915f97f0db
Joshua <atsujoshua67@gmail.com>
2024-03-26 16:47:13 -0700

    print elements if list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_dlistint - print elements of a double linked list
+ * @h: pointer to the first node in the list
+ * Return: number of elements
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int count = 0;
+
+	if (h == NULL)
+		return 0;
+	while (h->next != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+			count += 1;
+	}
+	printf("%d\n", h->n);
+	h = h->next;
+	return (count + 1);
+}
