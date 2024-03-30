6ae454f68c4de572f763aec2489f1666789b114a
Joshua <atsujoshua67@gmail.com>
2024-03-29 17:30:06 -0700

    add all elements in dlist

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -11,8 +11,10 @@ int sum_dlistint(dlistint_t *head)
 	dlistint_t *ptr;
 	int sum = 0;
 
+	if (head == NULL)
+		return (0);
 	ptr = head;
-	if (ptr != NULL)
+	while (ptr != NULL)
 	{
 		sum = sum + ptr->n;
 		ptr = ptr->next;
