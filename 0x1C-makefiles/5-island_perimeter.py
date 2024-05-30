aa724d12abe01bb7f65b97eee276f28e9accb60a
Joshua <atsujoshua67@gmail.com>
2024-05-30 02:45:19 -0700

    makefile advanced

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""a function def island_perimeter(grid)"""
+
+
+def island_perimeter(grid):
+    """returns the perimeter of the island described in grid
+    grid: is a list of integers"""
+    #height = []
+    #width = []
+    perimeter = len(grid)
+    return perimeter
