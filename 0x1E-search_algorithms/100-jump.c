c6cf592d088626528f88ff70f7a3851be611a29e
Joshua <atsujoshua67@gmail.com>
2024-07-20 06:14:05 -0700

    Jump search

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -21,7 +21,7 @@ int jump_search(int *array, size_t size, int value)
 	while (i < size)
 	{
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-		if (array[j] >= value)
+		if (array[j] >= value || j > size)
 		{
 			printf("Value found between indexes [%ld] and [%ld]\n",
 				i, j);
